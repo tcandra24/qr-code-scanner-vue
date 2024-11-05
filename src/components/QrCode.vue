811d217e8ce7c39ef14fc13a781e510166e4fade
tcandra24 <tcandra007@gmail.com>
2024-11-05 09:58:11 +0700

    Add isUrl Function & QrCode Component

---
+++ src/components/QrCode.vue
@@ -0,0 +1,158 @@
+<script setup>
+import { ref, computed } from "vue";
+import { QrcodeStream } from "vue-qrcode-reader";
+
+const emit = defineEmits(["onScan"]);
+
+const error = ref("");
+
+function detect(detectedCodes) {
+  const arrayResult = detectedCodes.map((code) => code.rawValue);
+  emit("onScan", arrayResult[0]);
+}
+
+function onError(err) {
+  error.value = `[${err.name}]: `;
+
+  if (err.name === "NotAllowedError") {
+    error.value += "you need to grant camera access permission";
+  } else if (err.name === "NotFoundError") {
+    error.value += "no camera on this device";
+  } else if (err.name === "NotSupportedError") {
+    error.value += "secure context required (HTTPS, localhost)";
+  } else if (err.name === "NotReadableError") {
+    error.value += "is the camera already in use?";
+  } else if (err.name === "OverconstrainedError") {
+    error.value += "installed cameras are not suitable";
+  } else if (err.name === "StreamApiNotSupportedError") {
+    error.value += "Stream API is not supported in this browser";
+  } else if (err.name === "InsecureContextError") {
+    error.value +=
+      "Camera access is only permitted in secure context. Use HTTPS or localhost rather than HTTP.";
+  } else {
+    error.value += err.message;
+  }
+}
+
+const selectedConstraints = ref({ facingMode: "environment" });
+const defaultConstraintOptions = [
+  { label: "rear camera", constraints: { facingMode: "environment" } },
+  { label: "front camera", constraints: { facingMode: "user" } },
+];
+const constraintOptions = ref(defaultConstraintOptions);
+
+async function onCameraReady() {
+  // NOTE: on iOS we can't invoke `enumerateDevices` before the user has given
+  // camera access permission. `QrcodeStream` internally takes care of
+  // requesting the permissions. The `camera-on` event should guarantee that this
+  // has happened.
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const videoDevices = devices.filter(({ kind }) => kind === "videoinput");
+
+  constraintOptions.value = [
+    ...defaultConstraintOptions,
+    ...videoDevices.map(({ deviceId, label }) => ({
+      label: `${label} (ID: ${deviceId})`,
+      constraints: { deviceId },
+    })),
+  ];
+
+  error.value = "";
+}
+
+function paintOutline(detectedCodes, ctx) {
+  for (const detectedCode of detectedCodes) {
+    const [firstPoint, ...otherPoints] = detectedCode.cornerPoints;
+
+    ctx.strokeStyle = "red";
+
+    ctx.beginPath();
+    ctx.moveTo(firstPoint.x, firstPoint.y);
+    for (const { x, y } of otherPoints) {
+      ctx.lineTo(x, y);
+    }
+    ctx.lineTo(firstPoint.x, firstPoint.y);
+    ctx.closePath();
+    ctx.stroke();
+  }
+}
+function paintBoundingBox(detectedCodes, ctx) {
+  for (const detectedCode of detectedCodes) {
+    const {
+      boundingBox: { x, y, width, height },
+    } = detectedCode;
+
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "#007bff";
+    ctx.strokeRect(x, y, width, height);
+  }
+}
+function paintCenterText(detectedCodes, ctx) {
+  for (const detectedCode of detectedCodes) {
+    const { boundingBox, rawValue } = detectedCode;
+
+    const centerX = boundingBox.x + boundingBox.width / 2;
+    const centerY = boundingBox.y + boundingBox.height / 2;
+
+    const fontSize = Math.max(12, (50 * boundingBox.width) / ctx.canvas.width);
+
+    ctx.font = `bold ${fontSize}px sans-serif`;
+    ctx.textAlign = "center";
+
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = "#35495e";
+    ctx.strokeText(detectedCode.rawValue, centerX, centerY);
+
+    ctx.fillStyle = "#5cb984";
+    ctx.fillText(rawValue, centerX, centerY);
+  }
+}
+
+const trackFunctionOptions = [
+  { text: "nothing (default)", value: undefined },
+  { text: "outline", value: paintOutline },
+  { text: "centered text", value: paintCenterText },
+  { text: "bounding box", value: paintBoundingBox },
+];
+const trackFunctionSelected = ref(trackFunctionOptions[1]);
+
+const barcodeFormats = ref({
+  aztec: false,
+  code_128: false,
+  code_39: false,
+  code_93: false,
+  codabar: false,
+  databar: false,
+  databar_expanded: false,
+  data_matrix: false,
+  dx_film_edge: false,
+  ean_13: false,
+  ean_8: false,
+  itf: false,
+  maxi_code: false,
+  micro_qr_code: false,
+  pdf417: false,
+  qr_code: true,
+  rm_qr_code: false,
+  upc_a: false,
+  upc_e: false,
+  linear_codes: false,
+  matrix_codes: false,
+});
+const selectedBarcodeFormats = computed(() => {
+  return Object.keys(barcodeFormats.value).filter(
+    (format) => barcodeFormats.value[format]
+  );
+});
+</script>
+
+<template>
+  <QrcodeStream
+    :constraints="selectedConstraints"
+    :track="trackFunctionSelected.value"
+    :formats="selectedBarcodeFormats"
+    @error="onError"
+    @detect="detect"
+    @camera-on="onCameraReady"
+  />
+</template>
--- src/view/home/Index.vue
+++ src/view/home/Index.vue
@@ -1,7 +1,7 @@
 <script setup>
 import LayoutDefault from "../../layouts/Default.vue";
-import { QrcodeStream } from "vue-qrcode-reader";
-import { ref, computed } from "vue";
+import QrCode from "../../components/QrCode.vue";
+import { ref } from "vue";
 
 import { useAppStore } from "../../stores/appStore";
 import { storeToRefs } from "pinia";
@@ -10,149 +10,17 @@ const store = useAppStore();
 const { base_url, scan_end_point, token } = storeToRefs(store);
 
 const resultScan = ref("");
-const error = ref("");
 
-function onScan(detectedCodes) {
-  console.log(base_url.value);
-  console.log(scan_end_point.value);
-  console.log(token.value);
-  const arrayResult = detectedCodes.map((code) => code.rawValue);
-  resultScan.value = arrayResult[0];
-}
+const onScan = (code) => {
+  resultScan.value = code;
+};
 
-function onError(err) {
-  error.value = `[${err.name}]: `;
+const isUrl = (value) => {
+  const urlRegex =
+    /^(?:(?:https?|ftp):\/\/)?(?:www\.)?[a-z0-9-]+(?:\.[a-z0-9-]+)+[^\s]*$/i;
 
-  if (err.name === "NotAllowedError") {
-    error.value += "you need to grant camera access permission";
-  } else if (err.name === "NotFoundError") {
-    error.value += "no camera on this device";
-  } else if (err.name === "NotSupportedError") {
-    error.value += "secure context required (HTTPS, localhost)";
-  } else if (err.name === "NotReadableError") {
-    error.value += "is the camera already in use?";
-  } else if (err.name === "OverconstrainedError") {
-    error.value += "installed cameras are not suitable";
-  } else if (err.name === "StreamApiNotSupportedError") {
-    error.value += "Stream API is not supported in this browser";
-  } else if (err.name === "InsecureContextError") {
-    error.value +=
-      "Camera access is only permitted in secure context. Use HTTPS or localhost rather than HTTP.";
-  } else {
-    error.value += err.message;
-  }
-}
-
-const selectedConstraints = ref({ facingMode: "environment" });
-const defaultConstraintOptions = [
-  { label: "rear camera", constraints: { facingMode: "environment" } },
-  { label: "front camera", constraints: { facingMode: "user" } },
-];
-const constraintOptions = ref(defaultConstraintOptions);
-
-async function onCameraReady() {
-  // NOTE: on iOS we can't invoke `enumerateDevices` before the user has given
-  // camera access permission. `QrcodeStream` internally takes care of
-  // requesting the permissions. The `camera-on` event should guarantee that this
-  // has happened.
-  const devices = await navigator.mediaDevices.enumerateDevices();
-  const videoDevices = devices.filter(({ kind }) => kind === "videoinput");
-
-  constraintOptions.value = [
-    ...defaultConstraintOptions,
-    ...videoDevices.map(({ deviceId, label }) => ({
-      label: `${label} (ID: ${deviceId})`,
-      constraints: { deviceId },
-    })),
-  ];
-
-  error.value = "";
-}
-
-function paintOutline(detectedCodes, ctx) {
-  for (const detectedCode of detectedCodes) {
-    const [firstPoint, ...otherPoints] = detectedCode.cornerPoints;
-
-    ctx.strokeStyle = "red";
-
-    ctx.beginPath();
-    ctx.moveTo(firstPoint.x, firstPoint.y);
-    for (const { x, y } of otherPoints) {
-      ctx.lineTo(x, y);
-    }
-    ctx.lineTo(firstPoint.x, firstPoint.y);
-    ctx.closePath();
-    ctx.stroke();
-  }
-}
-function paintBoundingBox(detectedCodes, ctx) {
-  for (const detectedCode of detectedCodes) {
-    const {
-      boundingBox: { x, y, width, height },
-    } = detectedCode;
-
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = "#007bff";
-    ctx.strokeRect(x, y, width, height);
-  }
-}
-function paintCenterText(detectedCodes, ctx) {
-  for (const detectedCode of detectedCodes) {
-    const { boundingBox, rawValue } = detectedCode;
-
-    const centerX = boundingBox.x + boundingBox.width / 2;
-    const centerY = boundingBox.y + boundingBox.height / 2;
-
-    const fontSize = Math.max(12, (50 * boundingBox.width) / ctx.canvas.width);
-
-    ctx.font = `bold ${fontSize}px sans-serif`;
-    ctx.textAlign = "center";
-
-    ctx.lineWidth = 3;
-    ctx.strokeStyle = "#35495e";
-    ctx.strokeText(detectedCode.rawValue, centerX, centerY);
-
-    ctx.fillStyle = "#5cb984";
-    ctx.fillText(rawValue, centerX, centerY);
-  }
-}
-
-const trackFunctionOptions = [
-  { text: "nothing (default)", value: undefined },
-  { text: "outline", value: paintOutline },
-  { text: "centered text", value: paintCenterText },
-  { text: "bounding box", value: paintBoundingBox },
-];
-const trackFunctionSelected = ref(trackFunctionOptions[1]);
-
-const barcodeFormats = ref({
-  aztec: false,
-  code_128: false,
-  code_39: false,
-  code_93: false,
-  codabar: false,
-  databar: false,
-  databar_expanded: false,
-  data_matrix: false,
-  dx_film_edge: false,
-  ean_13: false,
-  ean_8: false,
-  itf: false,
-  maxi_code: false,
-  micro_qr_code: false,
-  pdf417: false,
-  qr_code: true,
-  rm_qr_code: false,
-  upc_a: false,
-  upc_e: false,
-  linear_codes: false,
-  matrix_codes: false,
-});
-const selectedBarcodeFormats = computed(() => {
-  return Object.keys(barcodeFormats.value).filter(
-    (format) => barcodeFormats.value[format]
-  );
-});
+  return urlRegex.test(value);
+};
 </script>
 
 <template>
@@ -165,14 +33,7 @@ const selectedBarcodeFormats = computed(() => {
           <div
             className="relative border-8 overflow-hidden border-gray-600 bg-gray-60 rounded-3xl flex flex-col w-96 h-96 justify-center items-center bg-no-repeat bg-cover shadow-2xl"
           >
-            <QrcodeStream
-              :constraints="selectedConstraints"
-              :track="trackFunctionSelected.value"
-              :formats="selectedBarcodeFormats"
-              @error="onError"
-              @detect="onScan"
-              @camera-on="onCameraReady"
-            />
+            <QrCode @onScan="onScan" />
           </div>
         </div>
         <div className="relative flex gap-2">
@@ -184,7 +45,16 @@ const selectedBarcodeFormats = computed(() => {
             </button>
           </div>
         </div>
-        <div className="relative flex my-3">
+        <div v-if="isUrl(resultScan)" className="relative flex my-3">
+          <a :href="resultScan" target="_blank">
+            <span
+              class="bg-blue-100 text-blue-800 text-xl font-medium me-2 px-2.5 py-0.5 rounded"
+            >
+              {{ resultScan }}
+            </span>
+          </a>
+        </div>
+        <div v-else className="relative flex my-3">
           <textarea
             className="w-96 border-2 rounded border-gray-300"
             cols="30"
